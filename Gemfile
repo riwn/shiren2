a76b573b498cdb4196792510b94edaf8098a418c
riwn <ryoga0122@hotmail.co.jp>
2019-01-18 18:53:57 +0900

    申請フォームにバリデーションの追加

--- Gemfile
+++ Gemfile
@@ -56,4 +56,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+# Get Public Image Gem
 gem 'font-awesome-rails'
+
+# Image Upload
+gem 'carrierwave'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#Rank.create(name: 'あーる', dungeon: '最果てへの道99FTA', result: 4041, movie: 'http://yahoo.co.jp', resultdate: '2011-11-14')
-#Rank.create(name: 'よっつん', dungeon: 'カラクロTA', result: 2000, movie: 'https://www.google.co.jp/', resultdate: '2011-11-14')
