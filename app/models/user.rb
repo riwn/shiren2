52dbd54f53f7f3213f077712787fe1972b4c46d9
あーる <rskgc@R-MacBook-Pro.local>
2019-09-21 17:53:18 +0900

    プロフィールURLに変な内容を入れることが出来ないように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,34 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true, length: {maximum: 15}
   validates :introduction, length: {maximum: 1200}
+  validate :niconico_valid
+  validate :twitch_valid
+  validate :youtube_valid
+
+      # permissionとrejectionが両方Trueの時にエラーとする
+  def niconico_valid
+    if niconico != ""
+      if !niconico.include?("https://com.nicovideo.jp/community")
+        errors.add(:niconico, "コミュニティのURLを指定してください")
+      end
+    end
+  end
+
+  def twitch_valid
+    if twitch != ""
+      if !twitch.include?("https://www.twitch.tv")
+        errors.add(:twitch, "チャンネルのURLを指定してください")
+      end
+    end
+  end
+
+  def youtube_valid
+    if youtube != ""
+      if !youtube.include?("https://www.youtube.com/channel")
+        errors.add(:youtube, "のURLを指定してください")
+      end
+    end
+  end
 
   #登録時にメールアドレスを不要とする
   def email_required?
